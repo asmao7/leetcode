ff7db481c70e5de1d7c5f9b73149e19bf28a0984
soomyy <aaali@ualberta.ca>
2021-05-25 16:54:32 -0600

    merge 2 sorted arrays cpp

---
+++ MergeTwoSortedLists.cpp
@@ -0,0 +1,42 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* mergeTwoLists(ListNode* l1, ListNode* l2) {
+        ListNode* l3 = new ListNode();
+        ListNode* head = l3;
+        
+        while(l1 != nullptr && l2 != nullptr){
+            if(l1->val <= l2->val){
+                l3->next = l1;
+                l1 = l1->next;
+            }else{
+                l3->next = l2;
+                l2 = l2->next;
+            }
+            l3 = l3->next;  
+        }
+        
+        while(l1 != nullptr){
+            l3->next = l1;
+            l1 = l1->next;
+            l3 = l3->next;
+        }
+        
+        while(l2 != nullptr){
+            l3->next = l2;
+            l2 = l2->next;
+            l3 = l3->next;
+        }
+        
+        return head->next;
+    }
+};
